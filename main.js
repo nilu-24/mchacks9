13fce3c9d8d25f7bbebf22ea49b9a7b64d66fb3c
Mustakim Fil Umar <mustakimfilumar@Mustakims-MacBook-Air.local>
2022-01-22 15:50:22 -0500

    prompt user for n days

--- main.js
+++ main.js
@@ -6,12 +6,19 @@ function getLocation(){
 fetch("https://api.openweathermap.org/data/2.5/forecast?q="+location.value+"&appid=c9dbfe419caaad8dfe99e90d39c81b98")
 .then(response=>response.json())
 .then(data => {
+    n = prompt('Enter number of days: ')
     for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Temp").innerHTML = "Temp:" + Number(data.list[8*i].main.temp - 273.15).toFixed(1)+ "°";
-        document.getElementById("day" + (i+1) + "Feel").innerHTML = "Feel:" + Number(data.list[8*i].main.feels_like - 273.15).toFixed(1)+ "°";
-        document.getElementById("day" + (i+1) + "Con").innerHTML = String(data.list[8*i].weather[0].main);
-        document.getElementById("img"+(i+1)).src = "http://openweathermap.org/img/wn/"+String(data.list[8*i].weather[0].icon)+"@2x.png";
-        document.getElementById("day"+(i+1)).innerHTML = String(data.list[8*i].dt_txt);
+        if (i<n){
+            document.getElementById("day" + (i+1) + "Temp").innerHTML = "Temp:" + Number(data.list[8*i].main.temp - 273.15).toFixed(1)+ "°";
+            document.getElementById("day" + (i+1) + "Feel").innerHTML = "Feel:" + Number(data.list[8*i].main.feels_like - 273.15).toFixed(1)+ "°";
+            document.getElementById("day" + (i+1) + "Con").innerHTML = String(data.list[8*i].weather[0].main);
+            document.getElementById("img"+(i+1)).src = "http://openweathermap.org/img/wn/"+String(data.list[8*i].weather[0].icon)+"@2x.png";
+            document.getElementById("day"+(i+1)).innerHTML = String(data.list[8*i].dt_txt);
+        }
+        else{
+            document.getElementById("element").style.display = "none";
+        }
+        
     }
 })
 
